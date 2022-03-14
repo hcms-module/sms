6eecdcf62cc9fa46270cf7f6e4412ba2e28caa5a
zhlhuang <364626853@qq.com>
2022-03-14 16:23:20 +0800

    加入安装依赖

---
+++ Install/config.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name' => 'Sms',
+    'require' => [
+        'hcms_version' => '0.2.0',
+        'composer' => [
+            'overtrue/easy-sms' => '^2.1'
+        ],
+        'module' => []
+    ],
+    'version' => '1.0.0'
+];
